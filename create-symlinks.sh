745c19fd3e28d1685b516832bae54851c9dc4745
camerondoyle123 <cdoyle305@gmail.com>
2024-08-31 15:44:21 +1000

    feat(nix): new everything!

---
+++ create-symlinks.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/zsh
+
+ln -sf /etc/nixos/configuration.nix /home/cameron/GitHub/nixos-cam/configuration.nix;
+ln -sf /home/cameron/.config/home-manager/home.nix /home/cameron/GitHub/nixos-cam/home.nix;
+ln -sf /etc/nixos/hardware-configuration.nix /home/cameron/GitHub/nixos-cam/hardware-configuration.nix;
---
+++ home.nix
@@ -0,0 +1,1 @@
+/home/cameron/.config/home-manager/home.nix
