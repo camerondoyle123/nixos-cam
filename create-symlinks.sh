8e6d09d03267e8af25a66515b5ebe82e46d0160d
camerondoyle123 <cdoyle305@gmail.com>
2024-08-31 16:59:20 +1000

    fix(nix): changed shell to bash and removed hardware-config from running

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -1,5 +1,5 @@
-#!/usr/bin/zsh
+#!/usr/bin/bash
 
 ln -sf /home/cameron/GitHub/nixos-cam/configuration.nix /etc/nixos/configuration.nix;
 ln -sf /home/cameron/GitHub/nixos-cam/home.nix /home/cameron/.config/home-manager/home.nix;
-ln -sf /home/cameron/GitHub/nixos-cam/hardware-configuration.nix /etc/nixos/hardware-configuration.nix;
+# ln -sf /home/cameron/GitHub/nixos-cam/hardware-configuration.nix /etc/nixos/hardware-configuration.nix;
