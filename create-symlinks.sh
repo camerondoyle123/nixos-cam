c66216023f1afc9d5315b3698201955024ddc9ad
camerondoyle123 <cdoyle305@gmail.com>
2024-09-01 12:11:07 +1000

    fix(sym): make conditional statements for creating directories if they don't exist, alongside backups of original config

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/bash
 
+# if you haven't made a backup for configuration.nix, then do so
+if [ ! -f /etc/nixos/configuration-legacy.nix ]; then
+    echo "Legacy configuration.nix file not found, creating...";
+    mv /etc/nixos/configuration.nix /etc/nixos/configuration-legacy.nix;
+fi
+# link the config file
 ln -sf /home/cameron/GitHub/nixos-cam/configuration.nix /etc/nixos/configuration.nix;
+
+# for the hardware-configuration.nix, also make a backup
+if [ ! -f /etc/nixos/hardware-configuration-legacy.nix ]; then
+    echo "Legacy hardware-configuration.nix file not found, creating...";
+    mv /etc/nixos/hardware-configuration.nix /etc/nixos/hardware-configuration-legacy.nix;
+fi
+
+
+# create the home-manager directory if it doesn't exist already
+mkdir -p /home/cameron/.config/home-manager
 ln -sf /home/cameron/GitHub/nixos-cam/home.nix /home/cameron/.config/home-manager/home.nix;
-# ln -sf /home/cameron/GitHub/nixos-cam/hardware-configuration.nix /etc/nixos/hardware-configuration.nix;
+
