90d0596ef0bc3f19c6fd6b4594211213d4353b61
camerondoyle123 <cdoyle305@gmail.com>
2024-09-03 21:13:35 +1000

    feat(alacritty): made the alacritty config file

---
+++ home-manager/alacritty.nix
@@ -0,0 +1,32 @@
+{ pkgs, ... }:
+{
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      env.TERM = "xterm-256color";
+      window.padding = {
+        x = 24;
+        y = 24;
+      };
+      window.decorations = "none";
+      window.opacity = 0.7;
+      scrolling.history = 1000;
+      font = {
+        normal = {
+          family = "Terminess Nerd Font Mono";
+          style = "Regular";
+        };
+        bold = {
+          family = "Terminess Nerd Font Mono";
+          style = "Bold";
+        };
+        italic = {
+          family = "Terminess Nerd Font Mono";
+          style = "Italic";
+        };
+        size = 14;
+      };
+      shell.program = "zsh";
+    };
+  };
+}
